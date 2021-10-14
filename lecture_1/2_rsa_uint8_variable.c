0273581b90470d762c75a37a6e5cad9153ccaa3b
Stefan Hackenberg <mail@stefan-hackenberg.de>
2021-10-14 16:13:57 +0200

    Add rest of lecture 1

--- lecture_1/2_rsa_uint8_variable.c
+++ lecture_1/2_rsa_uint8_variable.c
@@ -1,27 +1,20 @@
 /*  -*- tab-width: 4; c-basic-offset: 4; c-file-style: "linux"; indent-tabs-mode: nil; -*-
  *
- * RSA exponentation demo with uint8 integers giving parameters:
- *   p = 13, q = 17, n = 221
- *   phi(n) = 12 * 16 = 192
- *   private_exponent = 5
- *   public_exponent = 77
+ * RSA exponentation demo with uint8 integers and variable parameters.
  */
 
 #include "hal.h"
 #include "simpleserial.h"
 #include <stdint.h>
 
-const uint8_t modulus = 221;
-const uint8_t private_exponent = 13;
-
-
 /**
  * RSA exponentation on uint8_t integers.
  */
-uint8_t rsa_uint8_fixed(uint8_t *input)
+uint8_t rsa_uint8(uint8_t *input)
 {
-    uint8_t exponent = private_exponent;
-    uint8_t message = 0xA0;
+    uint8_t message = input[0],
+        exponent = input[1],
+        modulus = input[2];
 
     trigger_high();
 
@@ -53,7 +46,7 @@ int main(void)
     trigger_setup();
 
     simpleserial_init();
-    simpleserial_addcmd('r', 0, rsa_uint8_fixed);
+    simpleserial_addcmd('r', 3 * sizeof(uint8_t), rsa_uint8);
     while(1)
         simpleserial_get();
 }
